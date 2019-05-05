82d67b00e807544e049017e3b9c0c342b7e8879d
Srndo <xsesta06@stud.fit.vutbr.cz>
2019-05-05 16:11:52 +0200

    a

---
+++ ${src.dir}/ija/project/common/Figure.java
@@ -0,0 +1,29 @@
+/**
+ * IJA 2018/2019
+ * Projekt
+ * @author Jan Ježek (xjezek15)
+ */
+
+package ija.project.common;
+
+public class Figure extends java.lang.Object implements IFigure
+{
+    private boolean isBlack;
+    private IFigure.Type type;
+
+    public Figure(boolean isBlack, IFigure.Type type) 
+    {
+        this.isBlack = isBlack;
+        this.type = type;
+    }
+
+    public boolean isBlack()
+    {
+        return this.isBlack;
+    }
+
+    public IFigure.Type getType() 
+    {
+        return this.type;
+    }
+}
